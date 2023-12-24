94456949f78887ccb4478d3ffe39c7adab035103
supratik <supratikpal53@gmail.com>
2023-12-24 16:52:36 +0530

    mongodb is updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const mongoURI = process.env.MONGO_CONNECTION_URI;
 
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, { 
+  useNewUrlParser: true,
+}).then(() => console.log("db is connected")).catch((err) => {
+  console.log("db is not connected");
+});
 
 const db = mongoose.connection;
 
